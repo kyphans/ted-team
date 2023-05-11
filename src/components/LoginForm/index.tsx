4e76780271cb17d489e6f0cd7be64ee6de14a3b8
Ky Phan <kyphanz1@gmail.com>
2023-05-12 00:39:51 +0700

    Fix UI

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -47,7 +47,7 @@ function LoginForm() {
           autoComplete="off"
         >
           <Form.Item name="phone" rules={[{ required: true, message: 'Please input your phone!' }]}>
-            <Input placeholder="Phone" />
+            <Input placeholder="Phone number" />
           </Form.Item>
           <Form.Item name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
             <Input.Password placeholder="Password" />
