b1fb070e657c4343ce1fac645ec40409ccaed97a
Ky Phan <kyphanz1@gmail.com>
2023-06-11 20:51:02 +0700

    .

---
+++ src/components/MemberTable/index.tsx
@@ -0,0 +1,128 @@
+import { Button, Space, Tag } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+import moment from 'moment';
+import { tw } from '../../common/utils/classUtil';
+
+import { BulbOutlined, DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import PrimaryTable, { PrimaryTableProps } from '../__common/custom/PrimaryTable';
+
+interface DataType {
+  key: number;
+  fullName: string;
+  mssv: string;
+  phone: string;
+  email: string;
+  generation: number;
+  joinedDate: Date;
+  leaveDate: Date;
+  isActive: boolean;
+  isDelete: boolean;
+  description: string;
+}
+
+interface MemberTableProps extends Omit<PrimaryTableProps, 'columns'> {
+  handleEditMemberForm: (record: DataType) => void;
+  handleViewMemberForm: (record: DataType) => void;
+}
+const generationFilter = () => {
+  let array = [];
+  for (let index = 1; index < 10; index++) {
+    array.push({
+      text: `${index}`,
+      value: `${index}`,
+    });
+  }
+  return array;
+};
+
+export default function MemberTable(props: MemberTableProps) {
+  const { rows, rowClassName, rowKey, loading, className } = props;
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'ID',
+      dataIndex: 'mssv',
+      width: 1,
+    },
+    {
+      title: 'Full Name',
+      dataIndex: 'fullName',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+    },
+    {
+      title: 'Generation',
+      dataIndex: 'generation',
+      filters: generationFilter(),
+      onFilter: (value: any, record) => record.generation.toString().includes(value),
+      sorter: (a, b) => a.generation - b.generation,
+    },
+    {
+      title: 'Joined Date',
+      sorter: (a, b) => moment(a.joinedDate).unix() - moment(b.joinedDate).unix(),
+      render: (_, { joinedDate }) => {
+        return joinedDate.toLocaleDateString('en-US');
+      },
+    },
+    {
+      title: 'Leave Date',
+      sorter: (a, b) => moment(a.joinedDate).unix() - moment(b.joinedDate).unix(),
+      render: (_, { leaveDate }) => {
+        return leaveDate.toLocaleDateString('en-US');
+      },
+    },
+    {
+      title: 'Status',
+      filters: [
+        {
+          text: 'Active',
+          value: true,
+        },
+        {
+          text: 'Deactivate',
+          value: false,
+        },
+      ],
+      onFilter: (value: any, record) => record.isActive === value,
+      render: (_, { isActive }) => (isActive ? <Tag color="green">Active</Tag> : <Tag color="red">Deactivate</Tag>),
+    },
+    {
+      title: 'Action',
+      width: 1,
+      render: (_, record) => (
+        <Space.Compact block>
+          <Button
+            type="default"
+            className="leading-[0] text-[#1677ff] border-[#1677ff]"
+            onClick={() => props.handleViewMemberForm(record)}
+          >
+            <BulbOutlined />
+          </Button>
+          <Button
+            type="default"
+            className="leading-[0] text-[#d46b08] border-[#d46b08]"
+            onClick={() => props.handleEditMemberForm(record)}
+          >
+            <EditOutlined />
+          </Button>
+          <Button type="default" className="leading-[0]" danger>
+            <DeleteOutlined />
+          </Button>
+        </Space.Compact>
+      ),
+    },
+  ];
+
+  return (
+    <PrimaryTable
+      rows={rows}
+      className={tw(className)}
+      rowClassName={tw(rowClassName)}
+      columns={columns}
+      dataSource={rows}
+      rowKey={rowKey}
+      loading={loading}
+    />
+  );
+}
--- src/pages/Collaborators/index.tsx
+++ src/pages/Collaborators/index.tsx
@@ -1,31 +1,15 @@
-import { Button, Divider, Space, Table, Tag, Typography, Input, Form } from 'antd';
-import type { ColumnsType } from 'antd/es/table';
-import fakeData from '../../common/fakeData/user.json';
-import formatUsersData from '../../common/utils/formatUsersData';
-import PrimaryButton from '../../components/__common/custom/PrimaryButton';
-import moment from 'moment';
-import { useCallback, useState, useTransition, memo, useMemo, useEffect } from 'react';
-import PrimaryModal from '../../components/__common/custom/PrimaryModal';
-import MemberForm from '../../components/MemberForm';
-import PrimaryForm from '../../components/__common/custom/PrimaryForm';
+import { Divider, Form, Input, Typography } from 'antd';
 import dayjs from 'dayjs';
+import { useCallback, useMemo, useState, useTransition } from 'react';
+import fakeData from '../../common/fakeData/user.json';
 import { tw } from '../../common/utils/classUtil';
+import formatUsersData from '../../common/utils/formatUsersData';
+import MemberForm from '../../components/MemberForm';
+import PrimaryButton from '../../components/__common/custom/PrimaryButton';
+import PrimaryForm from '../../components/__common/custom/PrimaryForm';
+import PrimaryModal from '../../components/__common/custom/PrimaryModal';
 
-import { DeleteOutlined, EditOutlined, BulbOutlined } from '@ant-design/icons';
-
-interface DataType {
-  key: number;
-  fullName: string;
-  mssv: string;
-  phone: string;
-  email: string;
-  generation: number;
-  joinedDate: Date;
-  leaveDate: Date;
-  isActive: boolean;
-  isDelete: boolean;
-  description: string;
-}
+import MemberTable from '../../components/MemberTable';
 
 function Collaborators() {
   const parseFullName = (fullName: string) => {
@@ -37,93 +21,6 @@ function Collaborators() {
       lastName: lastName,
     };
   };
-
-  const generationFilter = () => {
-    let array = [];
-    for (let index = 1; index < 10; index++) {
-      array.push({
-        text: `${index}`,
-        value: `${index}`,
-      });
-    }
-    return array;
-  };
-  const columns: ColumnsType<DataType> = [
-    {
-      title: 'ID',
-      dataIndex: 'mssv',
-      width: 1,
-    },
-    {
-      title: 'Full Name',
-      dataIndex: 'fullName',
-    },
-    {
-      title: 'Email',
-      dataIndex: 'email',
-    },
-    {
-      title: 'Generation',
-      dataIndex: 'generation',
-      filters: generationFilter(),
-      onFilter: (value: any, record) => record.generation.toString().includes(value),
-      sorter: (a, b) => a.generation - b.generation,
-    },
-    {
-      title: 'Joined Date',
-      sorter: (a, b) => moment(a.joinedDate).unix() - moment(b.joinedDate).unix(),
-      render: (_, { joinedDate }) => {
-        return joinedDate.toLocaleDateString('en-US');
-      },
-    },
-    {
-      title: 'Leave Date',
-      sorter: (a, b) => moment(a.joinedDate).unix() - moment(b.joinedDate).unix(),
-      render: (_, { leaveDate }) => {
-        return leaveDate.toLocaleDateString('en-US');
-      },
-    },
-    {
-      title: 'Status',
-      filters: [
-        {
-          text: 'Active',
-          value: true,
-        },
-        {
-          text: 'Deactivate',
-          value: false,
-        },
-      ],
-      onFilter: (value: any, record) => record.isActive === value,
-      render: (_, { isActive }) => (isActive ? <Tag color="green">Active</Tag> : <Tag color="red">Deactivate</Tag>),
-    },
-    {
-      title: 'Action',
-      width: 1,
-      render: (_, record) => (
-        <Space.Compact block>
-          <Button
-            type="default"
-            className="leading-[0] text-[#1677ff] border-[#1677ff]"
-            onClick={() => handleViewMemberForm(record)}
-          >
-            <BulbOutlined />
-          </Button>
-          <Button
-            type="default"
-            className="leading-[0] text-[#d46b08] border-[#d46b08]"
-            onClick={() => handleEditMemberForm(record)}
-          >
-            <EditOutlined />
-          </Button>
-          <Button type="default" className="leading-[0]" danger>
-            <DeleteOutlined />
-          </Button>
-        </Space.Compact>
-      ),
-    },
-  ];
 
   const [form] = Form.useForm();
   const initialData = useMemo(() => formatUsersData(fakeData), []); // Assuming fakeData is static
@@ -161,7 +58,6 @@ function Collaborators() {
       leaveDate: dayjs(leaveDate),
     });
   };
-
   const handleEditMemberForm = (value: any) => {
     setIsOpenModal(true);
     setIsEdit(true);
@@ -177,8 +73,6 @@ function Collaborators() {
       joinDate: dayjs(joinedDate),
       leaveDate: dayjs(leaveDate),
     });
-
-    // setOpenMemberFormModal(true);
   };
   const handleAddMemberForm = (value: any) => {
     form.resetFields();
@@ -187,7 +81,7 @@ function Collaborators() {
   };
   return (
     <>
-      <Typography.Title className="mt-5" level={4}>
+      <Typography.Title className="mt-5" level={3}>
         COLLABORATORS
       </Typography.Title>
       <div className="flex space-x-2">
@@ -214,11 +108,13 @@ function Collaborators() {
 
       <Divider className="mb-4 mt-3" />
       <div className="w-full overflow-x-scroll scrollbar-hide">
-        <Table
+        <MemberTable
           className={tw('[&_.ant-table-tbody]:bg-white')}
-          columns={columns}
+          rows={dataSource}
           dataSource={dataSource}
-          rowKey={({ key }) => key}
+          handleEditMemberForm={handleEditMemberForm}
+          handleViewMemberForm={handleViewMemberForm}
+          rowKey={({ key }: any) => key}
         />
       </div>
 
