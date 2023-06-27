b09189a4d932c8c08dd8a45055c738bad565d3ed
Ky Phan <51189352+kyphans@users.noreply.github.com>
2023-06-27 10:29:53 +0700

    Create UI

--- src/pages/URLShortener/index.tsx
+++ src/pages/URLShortener/index.tsx
@@ -1,13 +1,23 @@
-import { Input, Typography } from 'antd';
+import { Input, Typography, DatePicker } from 'antd';
+import type { DatePickerProps } from 'antd';
 import React, { useState } from 'react';
 import PrimaryButton from '../../components/__common/custom/PrimaryButton';
 import { doc, setDoc, addDoc, collection } from 'firebase/firestore';
 import db from '../../common/firebase/firebase';
 
 interface URLShortenerProps {}
+type data = {
+  userId: string;
+  url: string;
+  slug: string;
+  customSlug?: string;
+  dateExpiry?: string;
+  description?: string;
+};
 
 function URLShortener(props: URLShortenerProps) {
   const [inputValue, setInputValue] = useState<string>('');
+  const [customSlug, setCustomSlug] = useState<string>('');
 
   const handleSubmit = async () => {
     try {
@@ -20,6 +30,10 @@ function URLShortener(props: URLShortenerProps) {
     }
   };
 
+  const onChange: DatePickerProps['onChange'] = (date, dateString) => {
+    console.log(date, dateString);
+  };
+
   return (
     <div>
       <div className="flex justify-between mb-3">
@@ -27,8 +41,25 @@ function URLShortener(props: URLShortenerProps) {
           URL Shortener
         </Typography.Title>
       </div>
-      <div className='mb-3'>
-        <Input addonBefore="http://" placeholder="URL link" onChange={(e) => setInputValue(e.target.value)} />
+      <div className="mb-3 [&_.ant-input]:h-[35px] [&_.ant-picker]:h-[35px] [&_.ant-picker]:w-full">
+        <Input className="mb-3" disabled />
+        <Input 
+          className="mb-3"
+          addonBefore="http://"
+          placeholder="URL link"
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <div className="flex space-x-3">
+          <div className="flex-1">
+            <Input addonBefore="Custom link" placeholder="test.com/" onChange={(e) => setCustomSlug(e.target.value)} />
+          </div>
+          <div className="flex-1">
+            <DatePicker className='' onChange={onChange} />
+          </div>
+          <div className="flex-1">
+            <Input addonBefore="Description" placeholder="" onChange={(e) => setCustomSlug(e.target.value)} />
+          </div>
+        </div>
       </div>
       <PrimaryButton onClick={handleSubmit}>Submit</PrimaryButton>
     </div>
