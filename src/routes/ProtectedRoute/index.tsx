daded8fd76be5641636f55b0953072df3511f1ea
Ky Phan <51189352+kyphans@users.noreply.github.com>
2023-07-19 16:55:17 +0700

    Enable validate auth

--- src/routes/ProtectedRoute/index.tsx
+++ src/routes/ProtectedRoute/index.tsx
@@ -4,9 +4,9 @@ import { useAuth } from '../../context/AuthContext';
 export const ProtectedRoute = ({ children }: any) => {
   const { user } = useAuth();
   const userData = !!user && JSON.parse(user);
-  // if (!userData?.info || !userData?.token) {
-  //   // User is not authenticated
-  //   return <Navigate to="/login" />;
-  // }
+  if (!userData?.info || !userData?.token) {
+    // User is not authenticated
+    return <Navigate to="/login" />;
+  }
   return children;
 };
